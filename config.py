8c9057d725172526f3117ee5d22fdcadd9a6737d
JuliFed <fedorchenkojuli@gmail.com>
2018-06-12 15:32:47 +0300

    Rebased project

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-import os
 from datetime import timedelta
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'super_secret_key'
 JWT_EXPIRATION_DELTA = timedelta(seconds=900)
-
+SECRET_KEY = 'super_secret_key'
+LIKES_PER_HOUR_LIMIT = 5
